22f8edd2ad8cc4f29c9762a32d368060986d8c81
Cosmin Miroiu <miroiucosmin93@gmail.com>
2022-03-11 09:20:04 +0200

    Fine-tuning

--- src/main/java/com/finalproject/springbootfoodapp/config/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/finalproject/springbootfoodapp/config/JwtAuthenticationEntryPoint.java
@@ -15,6 +15,6 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException) throws IOException {
 
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized.");
     }
 }
